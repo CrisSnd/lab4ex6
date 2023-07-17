251d3fd6a191b6ce62ba9ae4ab4f2dd8eb3d53e1
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-17 21:09:59 +0300

    Add project files.

---
+++ lab4ex6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*  Scrieti o functie recursiva care va calcula al n-lea element din sirul lui Fibonacci, n fiind citit de la tastatura, apelati-o si afisati-I rezultatul.*/
+
+
+            Console.Write("Numarul n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numar " + n + " " + "sir Fibonacci" +" "+ "este: " + " " + NrFibonacci(n));
+        }
+
+
+
+        static int NrFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+          
+               return NrFibonacci(n - 1) + NrFibonacci(n - 2);
+            
+  
+        }
+    }
+}
